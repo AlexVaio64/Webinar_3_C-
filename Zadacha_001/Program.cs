180160d90aba5c16a95c8495d78415b9733ba2c9
Michel Nekrasov <moscou@inbox.ru>
2022-10-20 20:36:21 +0300

    add Zadacha 001

---
+++ Zadacha_001/Program.cs
@@ -0,0 +1,45 @@
+﻿// Программа принимает на вход координаты точки (x y), 
+// Задача 17.
+
+// возвращаем _тип Имя Функции() {
+// код_функции
+// возврат результата
+// }
+//
+int GetQuarterNumber(int x, int y) 
+{
+        
+    
+        
+
+    if (x > 0 && y > 0)
+        return 1;
+
+    if (x < 0 && y > 0)
+        return 2;
+
+    if (x < 0 && y < 0)
+        return 3;
+
+    if (x > 0 && y < 0)
+        return 4;
+    
+    return -1;
+}
+Console.WriteLine("ВВедите координаты точки, числа X и Y");
+Console.WriteLine("Введите X: ");
+
+int x = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите Y: ");
+
+int y = int.Parse(Console.ReadLine()!);
+
+int result = GetQuarterNumber(x, y);
+
+if (result > 0)
+    Console.WriteLine("Номер четверти, в которой лежит точка: " + result);
+
+else
+
+    Console.WriteLine("Неверные данные вводв.");
